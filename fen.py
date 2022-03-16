92f78b46a3d2adebbb7183d231d1ada8594472c4
Filip22022 <filipgolyszny@gmail.com>
2022-03-16 15:51:49 +0100

    Piece sprites

--- fen.py
+++ fen.py
@@ -20,25 +20,25 @@ def load_state(fen_string: string='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w
                     case 'P':
                         config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/pawn_white.png')
                     case 'r':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/rook_black.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/rook_black-1.png')
                     case 'R':
-                        config.board[row_num][col_num].piece = piece.Piece("rook", row_num, col_num, is_white, 'graphics/rook_white.png')
+                        config.board[row_num][col_num].piece = piece.Piece("rook", row_num, col_num, is_white, 'graphics/rook_white-1.png')
                     case 'b':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/bishop_black.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/bishop_black-1.png')
                     case 'B':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/bishop_white.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/bishop_white-1.png')
                     case 'n':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/pawn_black.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/knight_black.png')
                     case 'N':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/pawn_white.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/knight_white.png')
                     case 'k':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/pawn_black.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/king_black.png')
                     case 'K':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/pawn_white.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/king_white.png')
                     case 'q':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/pawn_black.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/queen_black.png')
                     case 'Q':
-                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/pawn_white.png')
+                        config.board[row_num][col_num].piece = piece.Piece("pawn", row_num, col_num, is_white, 'graphics/queen_white.png')
                 col_num += 1
             elif x.isnumeric():
                 print(int(x)==8)
